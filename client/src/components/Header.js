fbc0104d93218e3c4ee76f72a22e4252857f2425
Zivile Riaukaite <zivilukas@192.168.0.190>
2020-03-23 18:22:22 +0100

    First commit

---
+++ client/src/components/Header.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <div className="row">
+            <div className="col s12">
+              <a href="#!" className="left brand-logo">
+                Emaily
+              </a>
+              <ul className="right">{this.renderContent()}</ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
