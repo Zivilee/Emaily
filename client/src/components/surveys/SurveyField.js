90aafdcdbe17b57483869963cd8eb8d509bf4dc0
Zivile <zivileriaukaite@gmail.com>
2020-04-02 17:36:34 +0200

    Survey form

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -2,11 +2,14 @@
 // label and text input
 import React from "react";
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
+      <input {...input} style={{ marginBottom: "5x" }} />
+      <div className="red-text" style={{ marginBottom: "20px" }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,15 @@
+//Below line gives a warning in browser due to some simbol, but code below is for javascript, to check invalid emails
+
+const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default emails => {
+  const invalidEmails = emails
+    .split(",")
+    .map(email => email.trim())
+    .filter(email => re.test(email) === false);
+
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+  return;
+};
